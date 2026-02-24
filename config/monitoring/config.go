169020e03a73cf4109255a9e2258ac4ccd609aa4
Veera Adithya Dittakavi <veera.adithya.dittakavi@oracle.com>
2026-02-24 12:29:15 -0600

    add examples for monitoring service

--- config/monitoring/config.go
+++ config/monitoring/config.go
@@ -19,24 +19,7 @@ package monitoring
 import "github.com/crossplane/upjet/pkg/config"
 
 func Configure(p *config.Provider) {
-	p.AddResourceConfigurator("oci_monitoring_alarm_history_collection", func(r *config.Resource) {
-		r.Version = "v1alpha1"
-		// Identifier for this resource is assigned by the provider. In other
-		// words it is not simply the name of the resource.
-		r.ExternalName = config.IdentifierFromProvider
-		r.References["compartment_id"] = config.Reference{
-			TerraformName: "oci_identity_compartment",
-		}
-		r.References["metric_compartment_id"] = config.Reference{
-			TerraformName: "oci_identity_compartment",
-		}
-	})
 	p.AddResourceConfigurator("oci_monitoring_alarm", func(r *config.Resource) {
-		r.Version = "v1alpha1"
-		r.ExternalName = config.IdentifierFromProvider
-		r.References["compartment_id"] = config.Reference{
-			TerraformName: "oci_identity_compartment",
-		}
 		r.References["metric_compartment_id"] = config.Reference{
 			TerraformName: "oci_identity_compartment",
 		}
